efa069bfa9814d1810e59efc3ec24376b82d6e00
24Roger <rogernieto24@gmail.com>
2021-02-20 12:53:56 -0600

    conexion a la base de datos

--- XO-Monkey-Business-App/src/server.js
+++ XO-Monkey-Business-App/src/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+const database = require('./database/database');
 const app = express();
 
 // capturar body
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
-app.use(require('./database.js'))
+// Conexión a Base de datos
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("DataBase Conected"))
+  .catch((e) => console.log("Error dataBase:", e));
+
 // iniciar server
 const PORT = process.env.PORT || 3030;
 app.listen(3030, () => {
